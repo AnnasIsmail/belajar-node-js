e426c338ce101caf972c53dd660bdd4506f059c8
Ngabjago <86274500+AnnasIsmail@users.noreply.github.com>
2022-08-25 23:26:23 +0700

    Update

--- coba.js
+++ coba.js
@@ -3,4 +3,23 @@ function printConsole(){
     console.log('node js ku');
 }
 
-module.exports = printConsole;
+const name = "Annas";
+
+const identifier = {
+    nama: 'Annas Ismail Muhammad',
+    umur: 12,
+    printIdentifier(){
+        return `Halo nama saya ${this.nama} umur saya ${this.umur}`;
+    }
+}
+
+class classIdentifier{
+    constructor(){
+        console.log('Ini Class Identifier')
+    }
+}
+
+module.exports.printConsole = printConsole;
+module.exports.name = name;
+module.exports.identifier = identifier;
+module.exports.classIdentifier = classIdentifier;
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const coba = require('./coba');
 
-coba();
+coba.printConsole()
+console.log(coba.name);
+console.log(coba.identifier.printIdentifier());
+new coba.classIdentifier;
 
 console.log('hallo annas');
