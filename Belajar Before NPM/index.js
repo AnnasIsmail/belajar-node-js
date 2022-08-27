b13b70ca75c7b8e9892f7c3b694889d876139bbb
Ngabjago <86274500+AnnasIsmail@users.noreply.github.com>
2022-08-28 00:16:02 +0700

    tutor 8

---
+++ Belajar Before NPM/index.js
@@ -0,0 +1,8 @@
+const coba = require('./coba');
+
+coba.printConsole()
+console.log(coba.name);
+console.log(coba.identifier.printIdentifier());
+new coba.classIdentifier;
+
+console.log('hallo annas');
--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-const coba = require('./coba');
-
-coba.printConsole()
-console.log(coba.name);
-console.log(coba.identifier.printIdentifier());
-new coba.classIdentifier;
-
-console.log('hallo annas');
+console.log('Hello World');
