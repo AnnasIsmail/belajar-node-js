af30dbc218387f824c1049f56961268940d4f5fb
Ngabjago <86274500+AnnasIsmail@users.noreply.github.com>
2022-10-22 13:26:12 +0700

    Last PLs

--- tutorial23/utils/db.js
+++ tutorial23/utils/db.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/Annas',
+// mongoose.connect('mongodb://127.0.0.1:27017/Annas',
+mongoose.connect('mongodb+srv://annas:annas123@cluster0.ey2dmu0.mongodb.net/annas',
  {
     useNewUrlParser: true, 
     useUnifiedTopology: true,
